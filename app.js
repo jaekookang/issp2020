b8f43596a22a5487cc63ea9ea2bed7e617fbef55
Jaekoo Kang <jkang@gradcenter.cuny.edu>
2020-12-07 13:09:09 -0500

    update gitignore

--- app.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-var app = express();
-
-// --- ROUTING
-app.get("/", function(request, response){
-    // Show index.html page
-    response.render("index.html");
-});
